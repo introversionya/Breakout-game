53646def3e801d669f87888009dd8cbe04d26981
Anton Demidenko <yaintroversivnost@gmail.com>
2021-08-21 03:59:11 +0500

    Движение ракетки

--- js/app.js
+++ js/app.js
@@ -8,18 +8,76 @@ let y = canvas.height - 30;
 let dx = 2;
 let dy = -2;
 
+let ballRadius = 10;
+
+let paddleHeight = 10;
+let paddleWidth = 75;
+let paddleX = (canvas.width - paddleWidth) / 2;
+
+let rightPressed = false;
+let leftPressed = false;
+
 function drawBall() {
   ctx.beginPath();
-  ctx.arc(x, y, 10, 0, Math.PI*2);
+  ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
   ctx.fillStyle = "#0095DD";
   ctx.fill();
   ctx.closePath();
 }
 
+function drawPaddle() {
+  ctx.beginPath();
+  ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
+  ctx.fillStyle = "#0095DD";
+  ctx.fill();
+  ctx.closePath();
+}
+
+function bounceWalls() {
+  if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
+    dx = -dx;
+  }
+
+  if (y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
+    dy = -dy;
+  }
+}
+
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
+
   drawBall();
+  drawPaddle();
+
   x += dx;
   y += dy;
+
+  bounceWalls();
+
+  if (rightPressed && paddleX < canvas.width - paddleWidth) {
+    paddleX += 3;
+  } else if (leftPressed && paddleX > 0) {
+    paddleX -= 3;
+  }
 }
+
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
+function keyDownHandler(e) {
+  if (e.keyCode == 39) {
+    rightPressed = true;
+  } else if (e.keyCode == 37) {
+    leftPressed = true;
+  }
+}
+
+function keyUpHandler(e) {
+  if (e.keyCode == 39) {
+    rightPressed = false;
+  } else if (e.keyCode == 37) {
+    leftPressed = false;
+  }
+}
+
 setInterval(draw, 10);
