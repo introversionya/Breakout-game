03f38b7fd8ebdb7587f9322e60bc5f0d09635629
Anton Demidenko <yaintroversivnost@gmail.com>
2021-08-21 02:37:23 +0500

    init commit

---
+++ js/app.js
@@ -0,0 +1,9 @@
+const canvas = document.getElementById("myCanvas");
+const ctx = canvas.getContext("2d");
+// =====================================
+
+ctx.beginPath();
+ctx.rect(160, 10, 100, 40);
+ctx.strokeStyle = "rgba(0, 0, 255, 0.5)";
+ctx.stroke();
+ctx.closePath();
